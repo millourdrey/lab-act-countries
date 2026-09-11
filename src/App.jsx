bb2ce1e7d268950b951f47bf15399f63e235acef
Millourd Rey G. De Castro <millourdreygdecastro@gmail.com>
2026-09-11 11:31:15 +0800

    Task 3: add basic route table for all four pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,23 @@ import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
-import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="countries" element={<CountriesPage />} />
-        <Route path="bucket-list" element={<BucketListPage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
+      <Route path="/" element={<Layout><HomePage /></Layout>} />
+      <Route
+        path="/countries"
+        element={<Layout><CountriesPage /></Layout>}
+      />
+      <Route
+        path="/bucket-list"
+        element={<Layout><BucketListPage /></Layout>}
+      />
+      <Route
+        path="/about"
+        element={<Layout><AboutPage /></Layout>}
+      />
     </Routes>
   );
 };
